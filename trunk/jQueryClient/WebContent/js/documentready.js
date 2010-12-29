0f7d2bdfdc1e1062ff8f6da2687038586bf881d6
jcraane <jcraane@caf66a3a-fc27-11dd-b556-9305e745af9e>
2010-12-29 11:02:04 +0000

    Added event delegation sample.

--- trunk/jQueryClient/WebContent/js/documentready.js
+++ trunk/jQueryClient/WebContent/js/documentready.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+	/* Wrap everything in a single element */
 	$('#loadroot').click(function(){
 		$('#listroot').load('listdata-root.html');
 	});
 	
 	$('#loadnonroot').click(function(){
 		$('#listnonroot').load('listdata-nonroot.html');
+	});
+	/* Wrap everything in a single element */
+	
+	/* Use event delegation */
+	$('select.option-selector').change(function(e){
+		var clicked = $(e.target);
+		var input = clicked.prev();
+		// Give selectors a context
+		// get selected text of combobox
+		var selectedText = $('option:selected', clicked).text().trim();
+		input.val(clicked.val()+selectedText);
 	});
 });
